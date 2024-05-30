3f7625048fe5cc5c0bf1e7830dd5442ec578a85e
jorge.lopezg <jorge.lopezg@ezcorp.com>
2024-05-30 16:30:57 -0600

    se agregaron comentarios para mas claridad

--- thresholds.py
+++ thresholds.py
@@ -1,6 +1,6 @@
 
 
-# Get thresholds for beginner mode
+# Obtener parametros para el modo principiante
 def get_thresholds_beginner():
 
     _ANGLE_HIP_KNEE_VERT = {
@@ -28,7 +28,7 @@ def get_thresholds_beginner():
 
 
 
-# Get thresholds for beginner mode
+# Obtener parametros para el modo PRO
 def get_thresholds_pro():
 
     _ANGLE_HIP_KNEE_VERT = {
--- utils.py
+++ utils.py
@@ -10,12 +10,25 @@ counter = cv2.imread('counter.png')
 counter = cv2.cvtColor(counter, cv2.COLOR_BGR2RGB)
 
 def draw_rounded_rect(img, rect_start, rect_end, corner_width, box_color):
+    """
+    Dibuja un rectángulo con esquinas redondeadas en una imagen.
+
+    Args:
+        img: Imagen donde se dibujará el rectángulo.
+        rect_start: Coordenada superior izquierda del rectángulo (x1, y1).
+        rect_end: Coordenada inferior derecha del rectángulo (x2, y2).
+        corner_width: Ancho de las esquinas redondeadas.
+        box_color: Color del rectángulo.
+
+    Returns:
+        Imagen con el rectángulo dibujado.
+    """
 
     x1, y1 = rect_start
     x2, y2 = rect_end
     w = corner_width
 
-    # draw filled rectangles
+    # Dibujar rectángulos rellenos
     cv2.rectangle(img, (x1 + w, y1), (x2 - w, y1 + w), box_color, -1)
     cv2.rectangle(img, (x1 + w, y2 - w), (x2 - w, y2), box_color, -1)
     cv2.rectangle(img, (x1, y1 + w), (x1 + w, y2 - w), box_color, -1)
@@ -23,7 +36,7 @@ def draw_rounded_rect(img, rect_start, rect_end, corner_width, box_color):
     cv2.rectangle(img, (x1 + w, y1 + w), (x2 - w, y2 - w), box_color, -1)
 
 
-    # draw filled ellipses
+    # Dibujar elipses rellenas
     cv2.ellipse(img, (x1 + w, y1 + w), (w, w),
                 angle = 0, startAngle = -90, endAngle = -180, color = box_color, thickness = -1)
 
@@ -40,6 +53,19 @@ def draw_rounded_rect(img, rect_start, rect_end, corner_width, box_color):
 
 
 def draw_dotted_line(frame, lm_coord, start, end, line_color):
+    """
+   Dibuja una línea de puntos en una imagen.
+
+   Args:
+       frame: Imagen donde se dibujará la línea.
+       lm_coord: Coordenadas de inicio de la línea.
+       start: Posición de inicio en el eje Y.
+       end: Posición de fin en el eje Y.
+       line_color: Color de la línea.
+
+   Returns:
+       Imagen con la línea dibujada.
+   """
     pix_step = 0
 
     for i in range(start, end+1, 8):
@@ -62,6 +88,27 @@ def draw_text(
     overlay_image = False,
     overlay_type = None
 ):
+
+    """
+    Dibuja un texto en una imagen, con la opción de superponer una imagen.
+
+    Args:
+        img: Imagen donde se dibujará el texto.
+        msg: Mensaje de texto.
+        width: Ancho del borde redondeado.
+        font: Tipo de fuente del texto.
+        pos: Posición del texto en la imagen (x, y).
+        font_scale: Escala de la fuente.
+        font_thickness: Grosor de la fuente.
+        text_color: Color del texto.
+        text_color_bg: Color de fondo del texto.
+        box_offset: Desplazamiento del cuadro de texto.
+        overlay_image: Si se debe superponer una imagen.
+        overlay_type: Tipo de imagen a superponer ("correct", "incorrect", "counter").
+
+    Returns:
+        Tamaño del texto.
+    """
 
     offset = box_offset
     x, y = pos
@@ -105,6 +152,17 @@ def draw_text(
 
 
 def find_angle(p1, p2, ref_pt = np.array([0,0])):
+    """
+   Calcula el ángulo entre dos puntos con respecto a un punto de referencia.
+
+   Args:
+       p1: Primer punto (x, y).
+       p2: Segundo punto (x, y).
+       ref_pt: Punto de referencia (x, y). Por defecto es el origen (0, 0).
+
+   Returns:
+       Ángulo en grados entre los puntos con respecto al punto de referencia.
+   """
     p1_ref = p1 - ref_pt
     p2_ref = p2 - ref_pt
 
@@ -118,6 +176,18 @@ def find_angle(p1, p2, ref_pt = np.array([0,0])):
 
 
 def get_landmark_array(pose_landmark, key, frame_width, frame_height):
+    """
+    Obtiene las coordenadas de un punto de referencia de una pose normalizadas al tamaño del marco.
+
+    Args:
+        pose_landmark: Resultado de los puntos de referencia de la pose.
+        key: Clave del punto de referencia a obtener.
+        frame_width: Ancho del marco.
+        frame_height: Altura del marco.
+
+    Returns:
+        Coordenadas del punto de referencia (x, y) en el marco.
+    """
 
     denorm_x = int(pose_landmark[key].x * frame_width)
     denorm_y = int(pose_landmark[key].y * frame_height)
@@ -127,6 +197,22 @@ def get_landmark_array(pose_landmark, key, frame_width, frame_height):
 
 
 def get_landmark_features(kp_results, dict_features, feature, frame_width, frame_height):
+    """
+   Obtiene las coordenadas de los puntos de referencia para una característica específica de la pose.
+
+   Args:
+       kp_results: Resultados de los puntos clave de la pose.
+       dict_features: Diccionario de características con las claves de los puntos de referencia.
+       feature: Característica específica a obtener ('nose', 'left', 'right').
+       frame_width: Ancho del marco.
+       frame_height: Altura del marco.
+
+   Returns:
+       Coordenadas de los puntos de referencia para la característica especificada.
+
+   Raises:
+       ValueError: Si la característica no es 'nose', 'left' o 'right'.
+   """
 
     if feature == 'nose':
         return get_landmark_array(kp_results, dict_features[feature], frame_width, frame_height)
@@ -154,6 +240,21 @@ def get_mediapipe_pose(
                         min_tracking_confidence = 0.5
 
                       ):
+
+    """
+    Configura y devuelve un objeto de pose de MediaPipe.
+
+    Args:
+        static_image_mode: Si se debe usar el modo de imagen estática.
+        model_complexity: Complejidad del modelo (0, 1, 2).
+        smooth_landmarks: Si se deben suavizar los puntos de referencia.
+        min_detection_confidence: Confianza mínima para la detección.
+        min_tracking_confidence: Confianza mínima para el seguimiento.
+
+    Returns:
+        Objeto de pose de MediaPipe configurado.
+    """
+
     pose = mp.solutions.pose.Pose(
                                     static_image_mode = static_image_mode,
                                     model_complexity = model_complexity,
